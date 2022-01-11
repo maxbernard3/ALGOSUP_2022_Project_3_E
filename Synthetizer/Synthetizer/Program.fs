b48f620625c25c6c503dcce66779d24722ac2a21
maxbernard3 <80251657+maxbernard3@users.noreply.github.com>
2022-01-11 09:15:14 +0100

    Updated waveform and addded test
    change the way sawtooth is generated
    addded triangle wave
    added test for all 4 waveform

--- Synthetizer/Synthetizer/Program.fs
+++ Synthetizer/Synthetizer/Program.fs
@@ -1,35 +1,48 @@
-﻿open System.IO
-open Microsoft.FSharp.Core.Operators
-/// Write WAVE PCM soundfile (8KHz Mono 8-bit)
-let write stream (data:byte[]) =
-    use writer = new BinaryWriter(stream)
-    // RIFF
-    writer.Write("RIFF"B)
-    let size = 36 + data.Length in writer.Write(size)
-    writer.Write("WAVE"B)
-    // fmt
-    writer.Write("fmt "B)
-    let headerSize = 16 in writer.Write(headerSize)
-    let pcmFormat = 1s in writer.Write(pcmFormat)
-    let mono = 1s in writer.Write(mono)
-    let sampleRate = 2000 in writer.Write(sampleRate) 
-    let byteRate = sampleRate in writer.Write(byteRate)
-    let blockAlign = 1s in writer.Write(blockAlign)
-    let bitsPerSample = 8s in writer.Write(bitsPerSample)
-    //data
-    writer.Write("data"B)
-    writer.Write(data.Length)
-    writer.Write(data)
-let sample x = (x + 1.)/2. * 255. |> byte 
-let PI = 3.141592653589
-let squarewave m x = if sin(float x/ (1. /float m))>=0.  then 
-                                1.
-                             else
-                                -1.
-let sawtooth m x= float x*2.*PI
+namespace Synth
 
-let sinu m x = 1.*cos(2.*PI/0.35*float x)
+    open System.Numerics
+    open System.IO
+    open System
 
-let data = Array.init 20000 (fun i -> squarewave 0.2 i |> sample) //change la durée
-let stream = File.Create("../../../square.wav")
-write stream data
+    module Waveform =
+        let pi = Math.PI
+
+        let write stream (data:byte[]) =
+            use writer = new BinaryWriter(stream)
+            // RIFF
+            writer.Write("RIFF"B)
+            let size = 36 + data.Length in writer.Write(size)
+            writer.Write("WAVE"B)
+            // fmt
+            writer.Write("fmt "B)
+            let headerSize = 16 in writer.Write(headerSize)
+            let pcmFormat = 1s in writer.Write(pcmFormat)
+            let mono = 1s in writer.Write(mono)
+            let sampleRate = 8000 in writer.Write(sampleRate)
+            let byteRate = sampleRate in writer.Write(byteRate)
+            let blockAlign = 1s in writer.Write(blockAlign)
+            let bitsPerSample = 8s in writer.Write(bitsPerSample)
+            // data
+            writer.Write("data"B)
+            writer.Write(data.Length)
+            writer.Write(data)
+
+        let sample x = (x + 1.)/2. * 255. |> byte
+
+        //need to pass along i, x changes the frequency
+        let sinWave i x  = sin ( float i * x)
+
+        let triangleWave i x = asin(sin(pi*(float i)*x))/(pi/2.)
+
+        let squareWave i x =
+            if sin(float i*x)>= 0.  then 
+                1.
+            else
+                -1.
+
+        let sawtoothWave i x = float i*(-1. * float x)
+
+        let data = Array.init 16000 (fun i -> sawtoothWave i 1. |> sample)
+        let stream = File.Create(@"/Users/maxbernard/Documents/F#/tone006.wav")
+        write stream data
+
---
+++ Synthetizer/Systhetizer.test/UnitTest1.fs
@@ -0,0 +1,55 @@
+﻿module Systhetizer.test
+open Synth
+
+open NUnit.Framework
+
+[<SetUp>]
+let Setup () =
+    ()
+
+[<Test>]
+let sinWaveformTest () =
+    let result = Waveform.sinWave 90 60.3
+    Assert.AreEqual(result, -0.99487262318918815)
+
+    let result = Waveform.sinWave 9 62.8
+    Assert.AreEqual(result, -0.28276702757104044)
+
+    let result = Waveform.sinWave 42 5.7
+    Assert.AreEqual(result, 0.59635959615697076)
+
+[<Test>]
+let triangleWaveTest () =
+    let result = Waveform.triangleWave 90 60.3
+    Assert.AreEqual(result, -4.612279887310475E-13)
+
+    let result = Waveform.triangleWave 9 62.8
+    Assert.AreEqual(result, -0.3999999999999424)
+
+    let result = Waveform.triangleWave 42 5.7
+    Assert.AreEqual(result, -0.79999999999999261)
+
+[<Test>]
+let squareWaveTest () =
+    let result = Waveform.squareWave 90 60.3
+    Assert.AreEqual(result, -1.0)
+
+    let result = Waveform.squareWave 9 62.8
+    Assert.AreEqual(result, -1.0)
+
+    let result = Waveform.squareWave 42 5.7
+    Assert.AreEqual(result, 1.0)
+
+[<Test>]
+let sawtoothWaveTest () =
+    let result = Waveform.sawtoothWave 90 60.3
+    Assert.AreEqual(result, -5427.0)
+
+    let result = Waveform.sawtoothWave 9 62.8
+    Assert.AreEqual(result, -565.19999999999993)
+
+    let result = Waveform.sawtoothWave 42 5.7
+    Assert.AreEqual(result, -239.40000000000001)
+
+
+    
