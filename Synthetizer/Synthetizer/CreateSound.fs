841358e507038559e18fd8e8400ab680f5dffa33
PaulNowak36 <91249965+PaulNowak36@users.noreply.github.com>
2022-01-13 15:15:29 +0100

    Updated sound Custom
    -added commentaries, triangle and square waves

--- Synthetizer/Synthetizer/CreateSound.fs
+++ Synthetizer/Synthetizer/CreateSound.fs
@@ -32,26 +32,31 @@ module Waveform =
 
     // SET YP THE DIFFERENT FORMS OF WAVES
     //need to pass along i, x changes the frequency
+
+    //algorithm to create an empty Wave returning no signal
+    let emptyWave i x = 0.
+
+    //algorithm to create a sin Wave
     let sinWave i x  = sin ( float i * x)
 
-    let triangleWave i x = asin(sin(pi*(float i)*x))/(pi/2.)
+    //algorithm to create a triangle Wave
+    let triangleWave i x = asin(sin(pi*(float i)*x))/pi
 
+    //algorithm to create a square Wave
     let squareWave i x =
         if sin(float i*x)>= 0.  then
             1.
         else
             -1.
 
+    //algorithm to create a sawtooth Wave
     let sawtoothWave i x = float i*(-1. * float x)
 
-    let emptyWave i x = 0.
-
-    //Merge the sinus and sawtooth waves together
+    //Merge the sinus and sawtooth algorithms together
     let sawtoothPlusSinu m x = 1.*cos(2.*pi/0.35*float x)*2.*pi
 
 
-    // CREATE ARRAYS CONTAINING EACH  WAVES DATA
-    let data = Array.init 20000 (fun i -> sawtoothWave i 1. |> sample)
+    // CREATE ARRAYS CONTAINING EACH WAVES DATA
 
     let emptyTest = Array.init 20000 (fun i -> emptyWave i 1. |> sample)
     //create an array of the empty wave data where we can change the duration
@@ -59,57 +64,54 @@ module Waveform =
     let sinTest = Array.init 20000 (fun i -> sinWave i 1. |> sample)
     //create an array of the sin wave data where we can change the duration
 
+    let triangleTest = Array.init 20000 (fun i -> triangleWave i 1. |> sample)
+    //create an array of the triangle wave data where we can change the duration
+
+    let squareTest = Array.init 20000 (fun i -> squareWave i 1. |> sample)
+    //create an array of the square wave data where we can change the duration
+
     let sawtoothTest = Array.init 20000 (fun i -> sawtoothWave i 1. |> sample)
     //create an array of the sawtooth wave data where we can change the duration
 
+    //SOUND CUSTOMIZATION
+    // return all the data from each waves in arrays
 
-
-
-    //PROGRAM TESTING
-
-    // let testArray = Array.init 4 (fun v -> v + 5)
-
-    let createSinWave = [| yield! sinTest |]
-    let createSawtoothWave = [| yield! sawtoothTest |]
     let createEmptyWave = [| yield! emptyTest |]
-
-    let inputs =
+    let createSinWave = [| yield! sinTest |]
+    let createTriangleWave = [| yield! triangleTest |]
+    let createSquareWave = [| yield! squareTest |]
+    let createSawtoothWave = [| yield! sawtoothTest |]
+    
+    // create a combined wave based on several waves and concat all the data in a single wave
+    let customizedWave =
         [| createSinWave
-           createSawtoothWave
-           createSinWave
+           createTriangleWave
+           createSquareWave
            createSawtoothWave |]
         |> Array.concat
-    
- 
-    //PROGRAM TESTING
-
-
-
-
-
-
-    //COMBINING WAVES
-
-    let combined = [| yield! sinTest ; yield! sawtoothTest; |] //combine 2 waves in one
-
-    let threeCombined = [| yield! sinTest ; yield! emptyTest ; yield! sawtoothTest; |] //combine 3 waves in one
 
     // CREATING WAV. FILES
 
+    //Empty wave sound
     let emptyStream = File.Create(@"C:\Users\PaulNOWAK\Desktop\Algosup\Computering\Projects\Project sound synthesis\ALGOSUP_2022_Project_3_E\Synthetizer\Synthetizer\Sounds\empty.wav")
-    write emptyStream emptyTest     //create a new .wav file
+    write emptyStream emptyTest     
 
+    //Sin wave sound
     let SinStream = File.Create(@"C:\Users\PaulNOWAK\Desktop\Algosup\Computering\Projects\Project sound synthesis\ALGOSUP_2022_Project_3_E\Synthetizer\Synthetizer\Sounds\newSin.wav")
-    write SinStream sinTest     //create a new .wav file
+    write SinStream sinTest     
 
+    //Triangle wave sound
+    let TriangleStream = File.Create(@"C:\Users\PaulNOWAK\Desktop\Algosup\Computering\Projects\Project sound synthesis\ALGOSUP_2022_Project_3_E\Synthetizer\Synthetizer\Sounds\Triangle.wav")
+    write TriangleStream triangleTest     
+
+    //Square wave sound
+    let SquareStream = File.Create(@"C:\Users\PaulNOWAK\Desktop\Algosup\Computering\Projects\Project sound synthesis\ALGOSUP_2022_Project_3_E\Synthetizer\Synthetizer\Sounds\Square.wav")
+    write SquareStream squareTest     
+
+    //Sawtooth wave sound
     let sawtoothStream = File.Create(@"C:\Users\PaulNOWAK\Desktop\Algosup\Computering\Projects\Project sound synthesis\ALGOSUP_2022_Project_3_E\Synthetizer\Synthetizer\Sounds\newSawtooth.wav")
-    write sawtoothStream sawtoothTest   //create a new .wav file
+    write sawtoothStream sawtoothTest   
 
-    let dobleStream = File.Create(@"C:\Users\PaulNOWAK\Desktop\Algosup\Computering\Projects\Project sound synthesis\ALGOSUP_2022_Project_3_E\Synthetizer\Synthetizer\Sounds\newSinSawtoothCombined.wav")
-    write dobleStream combined  //create a new .wav file
-
-    let tripleStream = File.Create(@"C:\Users\PaulNOWAK\Desktop\Algosup\Computering\Projects\Project sound synthesis\ALGOSUP_2022_Project_3_E\Synthetizer\Synthetizer\Sounds\SinEmptySawtoothCombined.wav")
-    write tripleStream threeCombined  //create a new .wav file
-
-    let severalStreams = File.Create(@"C:\Users\PaulNOWAK\Desktop\Algosup\Computering\Projects\Project sound synthesis\ALGOSUP_2022_Project_3_E\Synthetizer\Synthetizer\Sounds\severalSinsCombined.wav")
-    write severalStreams inputs  //create a new .wav file
+    //Customized wave sound
+    let severalStreams = File.Create(@"C:\Users\PaulNOWAK\Desktop\Algosup\Computering\Projects\Project sound synthesis\ALGOSUP_2022_Project_3_E\Synthetizer\Synthetizer\Sounds\customizedSound.wav")
+    write severalStreams customizedWave  
