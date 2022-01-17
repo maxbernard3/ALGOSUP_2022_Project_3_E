aa023d63a35376422511505a0b1a9b8d27706cee
maxbernard3 <80251657+maxbernard3@users.noreply.github.com>
2022-01-17 09:24:49 +0100

    Squashed commit of the following:
    commit bb02c1e1a771fa29b09b9251e9bccc31dc5a39cc
    Author: maxbernard3 <80251657+maxbernard3@users.noreply.github.com>
    Date:   Mon Jan 17 09:24:22 2022 +0100
    
        updated Program.fs
    
        Added chord
        Reworked convertion into .wave
    
    commit 8a4d47409e041de2d782e94d9e7a4d6ee1756307
    Author: maxbernard3 <80251657+maxbernard3@users.noreply.github.com>
    Date:   Fri Jan 14 11:57:24 2022 +0100
    
        Update Software Architecture.md
    
    commit c0afcbf02c577e29e46c0f619115d5dc03d819da
    Merge: 5a46a3c 9a67b59
    Author: maxbernard3 <80251657+maxbernard3@users.noreply.github.com>
    Date:   Fri Jan 14 11:24:58 2022 +0100
    
        Merge branch 'main' into Max
    
    commit 5a46a3ceadee0f29ba831d3b8be175297684d866
    Author: maxbernard3 <80251657+maxbernard3@users.noreply.github.com>
    Date:   Fri Jan 14 11:18:23 2022 +0100
    
        Test and dotnet6.0
    
    commit 5ab1b4de1f543a29f518839cfbeb39e93be5810c
    Merge: 67880d6 4cd3c4a
    Author: maxbernard3 <80251657+maxbernard3@users.noreply.github.com>
    Date:   Fri Jan 14 09:12:22 2022 +0100
    
        Merge branch 'main' into Max
    
    commit 67880d64e2a4a49e927af2ccd765e386ef206b9c
    Merge: 550dcfd 7f76a5b
    Author: maxbernard3 <80251657+maxbernard3@users.noreply.github.com>
    Date:   Thu Jan 13 11:42:44 2022 +0100
    
        Merge branch 'main' into Max
    
    commit 550dcfd993804f509369b5ff422a2ecb1b4847ad
    Author: maxbernard3 <80251657+maxbernard3@users.noreply.github.com>
    Date:   Tue Jan 11 10:21:52 2022 +0100
    
        Create Software Architecture.md

--- Synthetizer/Synthetizer/Program.fs
+++ Synthetizer/Synthetizer/Program.fs
@@ -4,11 +4,9 @@ namespace Synth
         open System.IO
 
         let pi = Math.PI
-        let freq = 10. // In Hertz
         let sampleRate = 44100 // In Hertz
-        let amplitude = 0.8
         let overdrive = 1.
-        let duration = 1. // In seconds
+        let duration = 2. // In seconds
         let pcmFormat = 1
         let nbChannels = 1
         let bytesPerSample = 2
@@ -18,80 +16,49 @@ namespace Synth
             if x > 1. * multiplicator then 1. * multiplicator else
             x
 
-        let sinWave frequence amplitude t  =
-            amplitude * sin (2. * pi * t * frequence)
+        let makeChord waves =
+            let x = waves |>List.sum
+            x/float waves.Length 
 
-        let sawWave frequence amplitude  t =
-            2. * amplitude * (t * frequence - floor (0.5 +  t * frequence))
+        let sinWave frequence amplitude t  =
+            amplitude * sin (2. * pi * t * frequence / float sampleRate)
+
+        let sawWave frequence amplitude t =
+            2. * amplitude * (float t * frequence/float sampleRate - floor (0.5 +  t * frequence/float sampleRate))
 
         let squareWave frequence amplitude t =
-            amplitude * float (sign (sin (2. * pi * t * frequence)))
+            amplitude * float (sign (sin (2. * pi * t * frequence/float sampleRate)))
 
         let triangleWave frequence amplitude t =
-            2. * amplitude * asin (sin (2. * pi * t * frequence)) / pi
+            2. * amplitude * asin (sin (2. * pi * t * frequence/float sampleRate)) / pi
 
-        let doubleWave frequence amplitude t =
-            (sinWave frequence amplitude t) * (sinWave 440. amplitude t)
+        open System.IO
 
-        let bitsPerSample = bytesPerSample * 8
-
-        /// Write the WAVE PCM file
-        let write stream (data: byte []) =
-            let byteRate = sampleRate * nbChannels * bytesPerSample
-            let blockAlign = uint16 (nbChannels * bytesPerSample)
-
+        /// Write WAVE PCM soundfile 
+        let write stream (data:byte[]) =
             use writer = new BinaryWriter(stream)
             // RIFF
             writer.Write("RIFF"B)
-            writer.Write(36 + data.Length) // File size
+            let size = 36 + data.Length in writer.Write(size)
             writer.Write("WAVE"B)
             // fmt
             writer.Write("fmt "B)
-            writer.Write(16) // Header size
-            writer.Write(pcmFormat)
-            writer.Write(uint16 nbChannels)
-            writer.Write(sampleRate)
-            writer.Write(byteRate)
-            writer.Write(blockAlign)
-            writer.Write(uint16 bitsPerSample)
+            let headerSize = 16 in writer.Write(headerSize)
+            let pcmFormat = 1s in writer.Write(pcmFormat)
+            let mono = 1s in writer.Write(mono)
+            let sampleRate = sampleRate in writer.Write(sampleRate)
+            let byteRate = sampleRate in writer.Write(byteRate)
+            let blockAlign = 1s in writer.Write(blockAlign)
+            let bitsPerSample = 8s in writer.Write(bitsPerSample)
             // data
             writer.Write("data"B)
             writer.Write(data.Length)
             writer.Write(data)
 
-        /// Read WAVE PCM soundfile
-        let read stream =
-            use reader = new BinaryReader(stream)
+        let sample x = (x + 1.)/2. * 255. |> byte 
+        let data = Array.init (int (float sampleRate * duration)) (fun i -> makeChord [(sinWave 5 1 i); (triangleWave 5 1 i)] |> sample)
+        let stream = File.Create("tone.wav")
 
-            reader.ReadBytes(20) |> ignore // Header
-            let pcm = reader.ReadInt16()
-            let nbChannels = reader.ReadUInt16()
-            let sampleRate = reader.ReadInt32()
-            let byteRate = reader.ReadInt32()
-            let blockAlign = reader.ReadInt16()
-            let bitsPerSample = reader.ReadUInt16()
-            // data
-            reader.ReadBytes(4) |> ignore
-            let dataLength = reader.ReadInt32()
-            let data = reader.ReadBytes(dataLength)
+        //let result = read (File.Open("toneSquare.wav", FileMode.Open))
 
-            let duration = float dataLength / float sampleRate
-            sampleRate, duration, nbChannels, data
-
-        let generate func =
-            let size = int (duration * float sampleRate)
-            let toBytes x =
-                let unitary = (x + 2. ** (float bytesPerSample - 1.)) / 2.
-                let upscaled = round (unitary * (256. ** (float bytesPerSample))) - (if unitary = 1. then 1. else 0.)
-                [ for k in 0..(bytesPerSample-1) do byte (upscaled/(256.**float k)) ]
-
-            let getData = float >> (fun x -> (x / float sampleRate)) >> func (freq/2.) amplitude >> makeOverdrive overdrive >> toBytes
-            [ for i in 0 .. (size - 1) do yield! getData i ] |> Array.ofList
-
-        write (File.Create("toneSin.wav")) (generate sinWave)
-        write (File.Create("toneSquare.wav")) (generate squareWave)
-        write (File.Create("toneTriangle.wav")) (generate triangleWave)
-        write (File.Create("toneSaw.wav")) (generate sawWave)
-        write (File.Create("toneDouble.wav")) (generate doubleWave)
-
-        let result = read (File.Open("toneSquare.wav", FileMode.Open))
+        write stream data
