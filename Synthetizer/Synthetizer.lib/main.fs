4474610c11b75857ac12a26517512dcdfce370a7
maxbernard3 <80251657+maxbernard3@users.noreply.github.com>
2022-01-25 09:36:35 +0100

    Aurelien (#10)
    * library
    
    created the library project and organized the project
    
    * re added library
    
    * deleted Echo
    
    deleted an old useless file
    
    * updated Synthetizer.lib
    
    solved many issue with the switch to library
    
    * push early .lib
    
    Co-authored-by: aurelienfernandez <71769656+aurelienfernandez@users.noreply.github.com>

---
+++ Synthetizer/Synthetizer.lib/main.fs
@@ -0,0 +1,28 @@
+﻿namespace Synthetizer.lib
+    module Main =
+        let sampleRate = GlobalVar.sampleRate
+
+        open System
+        open System.IO
+        open System.Threading
+
+
+        let fusedData fullwave = 
+            fullwave |> Array.concat
+
+        let toBytes i = i |> Array.map(fun x -> (x + 1.)/2. * 255. |> byte)
+
+        let CreateWave (waveForm:string) (frequency:float) (amplitude:float) (duration:float) =
+            match waveForm with
+            | "sin" -> Array.init (int (float sampleRate * duration)) (fun i -> Waves.sinWave frequency amplitude i)
+            | "triangle" -> Array.init (int (float sampleRate * duration)) (fun i -> Waves.triangleWave frequency amplitude i)
+            | "sawtooth" -> Array.init (int (float sampleRate * duration)) (fun i -> Waves.sawWave frequency amplitude i)
+            | "square" -> Array.init (int (float sampleRate * duration)) (fun i -> Waves.squareWave frequency amplitude i)
+            | _ -> Array.init (int (float sampleRate * duration)) (fun i -> Waves.squareWave frequency amplitude i)
+
+
+        let saveFile wave (filePath:string) =
+            let waveBites = wave |> fusedData |> toBytes
+
+            let stream = File.Create($"{filePath}")
+            SoundPlayer.write stream waveBites
