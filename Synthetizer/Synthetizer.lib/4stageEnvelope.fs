8ba218c731f7c8e5c6eb1194920fd3c4cd12c3b9
maxbernard3 <80251657+maxbernard3@users.noreply.github.com>
2022-01-25 16:12:43 +0100

    updated 4stageEnvelope

--- Synthetizer/Synthetizer.lib/4stageEnvelope.fs
+++ Synthetizer/Synthetizer.lib/4stageEnvelope.fs
@@ -1,32 +1,21 @@
 ﻿namespace Synthetizer.lib
-    //module Envelope =
+    module Envelope =
+        open System.Collections.Generic
 
-        //open XPlot.Plotly
-        //let sampleRate = GlobalVar.bytesPerSample
-        //let floatSampleRate = float GlobalVar.bytesPerSample
-
-        //let attack amplitude A time i =
-        //    let fsr = float sampleRate
-        //    let x = fsr*A*time
-        //    (amplitude/x) * float i
-        //let decay amplitude decayVar D time i =
-        //    let fsr = float sampleRate
-        //    let y = fsr*D*time
-        //    amplitude - decayVar/y* float i
-        //let sustain amplitude S =
-        //    amplitude*S
-        //let release amplitude decayVar A time i =
-        //    let fsr = float sampleRate
-        //    let z = fsr*A*time
-        //    (amplitude - decayVar) - 0.5/z* i
+        let sampleRate = (GlobalVar.sampleRate)
     
-        //let envelope (wave:float[]) (attackVar:float) (decayVar:float) (sustainVar:float) (releaseVar:float) (sustainAmplitude)=
-        //    let attackArray = Array.init (int (floatSampleRate*A)) (fun i -> attack amplitude A time i)
-        //    let decayArray = Array.init (int (floatSampleRate*D)) (fun i -> decay amplitude decayVar D time i)
-        //    let sustainArray = Array.init (int (floatSampleRate*S)) (fun _ -> sustain amplitude S)
-        //    let releaseArray = Array.init (int (floatSampleRate*R)) (fun i -> release amplitude decayVar A time (float i) )
-        //    let finalData = GlobalFunc.fusedData[|attackArray; decayArray; sustainArray; releaseArray|]
-        //    finalData
+        let envelope (wave:float[]) (attackVar:float) (decayVar:float) (sustainVar:float) (releaseVar:float) (hold:float)=
+            let waveBuffer = List<float>()
+            for i=0 to (int (attackVar * float sampleRate)) do
+                waveBuffer.Add(wave.[i] * (float i / (attackVar * float sampleRate)))
 
-        //let showChart (data:float[]) =
-        //    data |> Chart.Line |> Chart.Show
+            for i=(int (attackVar* float sampleRate)) to (int ((attackVar + decayVar)* float sampleRate) - 1) do
+                waveBuffer.Add(wave.[i] * (hold + ((float i)*(1. - hold)) / (attackVar * float sampleRate)))
+
+            for i=(int ((attackVar + decayVar)* float sampleRate)) to (int ((attackVar + decayVar + sustainVar)* float sampleRate) - 1) do
+                waveBuffer.Add(wave.[i] * hold)
+
+            for i=(int ((attackVar + decayVar + sustainVar)* float sampleRate)) to (int ((attackVar + decayVar + sustainVar + releaseVar)* float sampleRate) - 1) do
+                waveBuffer.Add(wave.[i] * ((1. - hold) + ((float i)*(hold) / (attackVar * float sampleRate))))
+
+            waveBuffer.ToArray()
