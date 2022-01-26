1b177084ec9468caa8c6d7d564e29b1bf7c0b369
PaulNowak36 <91249965+PaulNowak36@users.noreply.github.com>
2022-01-26 09:05:14 +0100

    Updated new version
    Added the empty waves, and the delay and reduce functions in the new program

--- Synthetizer/Synthetizer.lib/main.fs
+++ Synthetizer/Synthetizer.lib/main.fs
@@ -41,6 +41,7 @@
             | "triangle" -> Array.init (int (float sampleRate * duration)) (fun i -> Waves.triangleWave frequency amplitude i)
             | "sawtooth" -> Array.init (int (float sampleRate * duration)) (fun i -> Waves.sawWave frequency amplitude i)
             | "square" -> Array.init (int (float sampleRate * duration)) (fun i -> Waves.squareWave frequency amplitude i)
+            | "empty" -> Array.init (int (float sampleRate * duration)) (fun i -> Waves.emptyWave frequency amplitude i)
             | _ -> Array.init (int (float sampleRate * duration)) (fun i -> Waves.squareWave frequency amplitude i)
 
 
--- Synthetizer/Synthetizer/Program.fs
+++ Synthetizer/Synthetizer/Program.fs
@@ -2,6 +2,18 @@
 open Synthetizer.lib
 module Sound1 =
 
-    let song = [|Main.CreateWave "sin" 200. 1. 1.5|]
+    let song = Main.CreateWave "sin" 200. 1. 1.5
 
-    Main.saveFile song "Tone.wave"
+    let song2 = Main.CreateWave "triangle" 200. 1. 1.5
+
+    let nothing = Main.CreateWave "empty" 200. 1. 1.5
+
+    let DelayedSong = GlobalFunc.delayedWave 8820 song
+
+    let ReducedSong = Filters.reduceAmplitude 2. song
+
+    let ChordSong = Filters.makeChord [|song; song2|]
+
+    let EverySong = GlobalFunc.fusedData[|song; song2; nothing; DelayedSong; ReducedSong; ChordSong|]
+
+    Main.saveFile [|EverySong|] "NewCombinedTone.wave"
