0f66eee096344261af1c3a00d84df15beb1dff66
maxbernard3 <80251657+maxbernard3@users.noreply.github.com>
2022-01-31 09:53:46 +0100

    Updated unit test

--- Synthetizer/Synthetizer.lib/4stageEnvelope.fs
+++ Synthetizer/Synthetizer.lib/4stageEnvelope.fs
@@ -8,18 +8,18 @@ namespace Synthetizer.lib
 
             let attack = new List<float>()
             for i=0 to (int (attackVar * float sampleRate)) do
-                attack.Add (wave.[i] * (-1.)*(float i / (attackVar * float sampleRate)))
+                attack.Add (wave.[i] *(float i / (attackVar * float sampleRate)))
 
             let decay = Array.create (int (float sampleRate * decayVar)) 0.
-            for i=(int(decayVar*float sampleRate) - 1) downto 1 do
-                Array.set decay (int (float sampleRate * decayVar) - i) (wave.[int (float sampleRate * (decayVar + attackVar)) - i] * (-1.) * (hold + ((float i)*(1. - hold)) / (decayVar * float sampleRate)))
+            for i=(int(decayVar*float sampleRate)) downto 1 do
+                Array.set decay (int (float sampleRate * decayVar) - i) (wave.[int (float sampleRate * (decayVar + attackVar)) - i] * (hold + ((float i)*(1. - hold)) / (decayVar * float sampleRate)))
 
             let sustain = new List<float>()    
-            for i=0 to int(sustainVar*float sampleRate) do
-                sustain.Add(wave.[int (float sampleRate * (decayVar + attackVar)) - i] * hold)
+            for i=0 to (int(sustainVar*float sampleRate) - 1) do
+                sustain.Add(wave.[int (float sampleRate * (decayVar + attackVar)) + i] * hold)
 
             let release = Array.create (int (float sampleRate * releaseVar)) 0.  
-            for i=(int(releaseVar*float sampleRate) - 1) downto 1 do
-                Array.set release (int (float sampleRate * releaseVar) - i) (wave.[int (float sampleRate * (decayVar + attackVar + sustainVar)) - i] * (-1.) * (((float i)*(hold)) / (sustainVar * float sampleRate)))
+            for i=(int(releaseVar*float sampleRate)) downto 1 do
+                Array.set release (int (float sampleRate * releaseVar) - i) (wave.[int (float sampleRate * (decayVar + attackVar + sustainVar)) - i] * (((float i)*(-1. * hold)) / (releaseVar * float sampleRate)))
 
             GlobalFunc.fusedData [|attack.ToArray(); decay; sustain.ToArray(); release|]
