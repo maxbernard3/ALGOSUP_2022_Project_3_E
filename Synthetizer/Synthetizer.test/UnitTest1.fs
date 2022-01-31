c2ef372c99b084570429b0471e8d13f1a985472a
maxbernard3 <80251657+maxbernard3@users.noreply.github.com>
2022-01-31 10:01:31 +0100

    Update UnitTest1.fs

--- Synthetizer/Synthetizer.test/UnitTest1.fs
+++ Synthetizer/Synthetizer.test/UnitTest1.fs
@@ -56,7 +56,7 @@ let envelopeTest () =
     let result = Envelope.envelope (Main.CreateWave "square" 650. 0.8 1.5) 0.25 0.25 0.75 0.25 0.6
 
     Assert.AreEqual(float32 0.00188662135, float32 result.[26])
-    Assert.AreEqual(float32 0.48, result.[35788])
+    Assert.AreEqual(float32 0.48, float32 result.[35788])
     Assert.AreEqual(float32 0.16936054421768707, float32 result.[2334])
 
 
