215bfa3e42c2f9c713856ddf7c5657db7c889762
maxbernard3 <80251657+maxbernard3@users.noreply.github.com>
2022-02-02 09:18:11 +0100

    reorganise test

--- Synthetizer/Synthetizer.test/WavesTest.fs
+++ Synthetizer/Synthetizer.test/WavesTest.fs
@@ -1,4 +1,4 @@
-﻿module Systhetizer.test
+﻿module Waves.test
 
 open Synthetizer.lib
 open NUnit.Framework
@@ -50,13 +50,5 @@ let sawtoothWaveTest () =
 
     let result = Waves.sawWave 42. 5.7 12
     Assert.AreEqual(float32 0.13028571, float32 result)
-
-[<Test>]
-let envelopeTest () =
-    let result = Envelope.envelope (Main.CreateWave "square" 650. 0.8 1.5) 0.25 0.25 0.75 0.25 0.6
-
-    Assert.AreEqual(float32 0.00188662135, float32 result.[26])
-    Assert.AreEqual(float32 0.48, float32 result.[35788])
-    Assert.AreEqual(float32 0.16936054421768707, float32 result.[2334])
 
 
